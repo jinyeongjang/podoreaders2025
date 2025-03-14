c071fa0d21003d502ef8dffc9c5cbf05e784d241
jinyeongjang <wlsdud4222@gmail.com>
2025-03-14 20:41:45 +0900

    📦 Chore: TailwindCSS 초기 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        xs: { max: '639px' },
+        xxs: { max: '375px' },
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+      spacing: {
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
       },
     },
   },
