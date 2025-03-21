0b1faca4070375dc79ee3c80f61a1698e1aa637a
jinyeongjang <wlsdud4222@gmail.com>
2025-03-21 11:02:58 +0900

    🎨 Design: HomePageBanner 컴포넌트 디자인, 내용 수정

--- src/components/home/HomePageBanner.tsx
+++ src/components/home/HomePageBanner.tsx
@@ -4,8 +4,15 @@ import { FaBook } from 'react-icons/fa';
 export default function HomePageBanner() {
   return (
     <>
-      <div className="container mx-auto w-[640px] rounded-xl rounded-t-lg bg-slate-200 px-2 py-2 tracking-tighter shadow-xl xs:w-full xs:px-5 xs:py-2 xs:text-[15px]">
-        포도리더스 개편안내: 말씀온도 계산기가 새롭게 추가되었어요.
+      <div className="container mx-auto mb-4 w-[640px] rounded-xl border border-indigo-200 bg-indigo-50 px-5 py-5 tracking-tight shadow-md transition-shadow duration-300 hover:shadow-lg xs:w-full xs:p-5">
+        <h2 className="text-md font-semibold text-indigo-800">4월 업데이트</h2>
+        <p className="text-sm tracking-tighter text-gray-600">
+          메인페이지가 캠퍼스 선택화면으로 변경될 예정입니다.<br></br>* 업데이트 예정 캠퍼스: 기도 1 기도 2 / 말씀 1
+          말씀 2
+        </p>
+        <p className="mt-2 text-sm tracking-tighter text-gray-600">
+          캠퍼스 선택 후 가족장으로부터 발급받은 비밀번호를 입력해야만<br></br>캠퍼스 가족원 페이지 이용이 가능해요.
+        </p>
       </div>
 
       <motion.div className="container mx-auto mt-2 w-[640px] rounded-t-lg py-4 xs:w-[350px] xs:px-0 xs:py-2">
